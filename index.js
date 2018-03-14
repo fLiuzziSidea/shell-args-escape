ac338c561de11994bc601623995a646f85c49f30
Francesco Liuzzi <francesco.liuzzi@sideagorup.com>
2018-03-14 16:36:42 +0100

    escape di tutti i caratteri eccetto alfanumerici e _,-

---
+++ index.js
@@ -0,0 +1,19 @@
+function singleArgEscape(arg) {
+	if (typeof arg === 'string') {
+		return arg.replace(/[^a-zA-Z0-9_-]/g, function (charToEscape) {
+			return '\\' + charToEscape;
+		});
+	}
+	console.log('non è una stringa: ', arg);
+	return arg;
+}
+
+function shellArgsEscape(args) {
+  if (!Array.isArray(args)) {
+		args = [args];
+	}
+
+	return args.map(singleArgEscape).join(' ');
+}
+
+module.exports = shellArgsEscape;
