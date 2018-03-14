41cc2ac8a2eaf42c652208190265c80b09341a48
Francesco Liuzzi <francesco.liuzzi@sideagorup.com>
2018-03-14 16:56:56 +0100

    formatto il codice

--- index.js
+++ index.js
@@ -1,19 +1,19 @@
 function singleArgEscape(arg) {
-	if (typeof arg === 'string') {
-		return arg.replace(/[^a-zA-Z0-9_-]/g, function (charToEscape) {
-			return '\\' + charToEscape;
-		});
-	}
-	console.log('non è una stringa: ', arg);
-	return arg;
+  if (typeof arg === 'string') {
+    return arg.replace(/[^a-zA-Z0-9_-]/g, function (charToEscape) {
+      return '\\' + charToEscape;
+    });
+  }
+  // console.log('non è una stringa: ', arg);
+  return arg;
 }
 
 function shellArgsEscape(args) {
   if (!Array.isArray(args)) {
-		args = [args];
-	}
+    args = [args];
+  }
 
-	return args.map(singleArgEscape).join(' ');
+  return args.map(singleArgEscape).join(' ');
 }
 
 module.exports = shellArgsEscape;
